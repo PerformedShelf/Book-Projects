b022037482a7c38b6c141a7662dd9c60df3eda7a
Matt <Matt@DESKTOP-R42UFO5>
2016-05-01 23:40:50 +0100

    Initial upload

---
+++ CastDemo/CastDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3, j = 2;
+            float fraction;
+            fraction = (float)i / (float)j;
+            Console.WriteLine("Fraction: " + fraction);
+            Console.ReadLine();
+        }
+    }
+}
